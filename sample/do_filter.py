0c97ba54ea84df150b4c10796076e99e04959b54
jaccho <55819635+jaccho@users.noreply.github.com>
2019-10-21 15:11:15 +0800

    creat do_filter.py

---
+++ sample/do_filter.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+def _not_divisible(n):
+    return lambda x : x % n > 0
+
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n), it)
+        
+# 打印1000以内的素数:
+for n in primes():
+    if n < 10000:
+        print(n)
+    else:
+        break
